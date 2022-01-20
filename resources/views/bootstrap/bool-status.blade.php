6298619fb7bad7f232849c42c38588dc9e6f94d5
Perla <perlad0015@gmail.com>
2022-01-20 11:37:03 -0400

    fix issue in bool status

--- resources/views/bootstrap/bool-status.blade.php
+++ resources/views/bootstrap/bool-status.blade.php
@@ -1,12 +1,12 @@
 <td>
-    @if($itemValue)
-        <a style="padding-left: 8px; padding-right: 4px;cursor: pointer;" wire:click="emitStatus(`{{$event}}`, {{$id}}, 1)">
-            <i class="fa fa-check" aria-hidden="true" style="color:darkgreen; "></i>
+    @if(!$itemValue)
+        <a style="padding-left: 8px; padding-right: 4px;cursor: pointer;" wire:click="emitStatus(`{{$event}}`,{{$id}}, 1)">
+            <i class="fa fa-times" style="color: red" aria-hidden="true"></i>
         </a>
     @else
-        <a style="padding-left: 8px; padding-right: 4px;cursor: pointer;" wire:click="emitStatus(`{{$event}}`,{{$id}}, 0)">
-            <i class="fa fa-times" style="color: red" aria-hidden="true"></i>
-
+        <a style="padding-left: 8px; padding-right: 4px;cursor: pointer;" wire:click="emitStatus(`{{$event}}`, {{$id}}, 0)">
+            <i class="fa fa-check" aria-hidden="true" style="color:darkgreen; "></i>
         </a>
+
     @endif
 </td>
