b92e0ee038c952606583c0661cea35729a3dfe20
Perla <perlad0015@gmail.com>
2022-01-26 10:47:58 -0400

    status option fix issue

--- resources/views/bootstrap/status-option.blade.php
+++ resources/views/bootstrap/status-option.blade.php
@@ -1,10 +1,10 @@
 <td>
-    @if($itemValue !== null)
-        <a style="padding-left: 8px; padding-right: 4px;cursor: pointer;" wire:click="emitStatus(`{{$event}}`,{{$id}}, false)">
+    @if($itemValue == null)
+        <a style="padding-left: 8px; padding-right: 4px;cursor: pointer;" wire:click="emitStatus(`{{$event}}`,{{$id}}, true)">
             <i class="fa fa-check" aria-hidden="true" style="color:darkgreen; "></i>
         </a>
     @else
-        <a style="padding-left: 8px; padding-right: 4px;cursor: pointer;" wire:click="emitStatus(`{{$event}}`, {{$id}}, true)">
+        <a style="padding-left: 8px; padding-right: 4px;cursor: pointer;" wire:click="emitStatus(`{{$event}}`, {{$id}}, false)">
             <i class="fa fa-times" style="color: red" aria-hidden="true"></i>
         </a>
     @endif
